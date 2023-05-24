77b51f2913050dfb4584bfaa2671ec7f7432e255
KentaGoto <kenta-goto@to-in.co.jp>
2023-05-24 12:13:53 +0900

    update main.go and add .gitignore

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
-	// Directory to be monitored.
-	dirToWatch := "."
+	log.Println("Monitering...")
 
-	// Create an instance of fsnotify.
+	// Create a new fsnotify watcher
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer watcher.Close()
 
+	done := make(chan bool)
+
+	// Create or open log.log for appending log output
+	file, err := os.OpenFile("log.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Create a new logger, writing to log.log
+	logger := log.New(file, "", log.LstdFlags)
+
+	// Run a separate goroutine to watch for events
 	go func() {
 		for {
 			select {
@@ -25,25 +37,34 @@ func main() {
 				if !ok {
 					return
 				}
-				// Display event type and file name.
-				log.Println("event:", event)
-				if event.Has(fsnotify.Write) {
-					log.Println("modified file:", event.Name)
+				log.Println("Event:", event)
+				logger.Println("Event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("Modified file:", event.Name)
+					logger.Println("Modified file:", event.Name)
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
 					return
 				}
-				// Display errors.
-				fmt.Println("error:", err)
+				log.Println("Error:", err)
+				logger.Println("Error:", err)
 			}
 		}
 	}()
 
-	// Add directories to be monitored.
-	err = watcher.Add(dirToWatch)
-	if err != nil {
-		log.Fatal(err)
+	// Add multiple directories to the watcher
+	directories := []string{
+		"/path/to/your/directory1",
+		"/path/to/your/directory2",
+		// You can add more directories here
 	}
-	<-make(chan struct{})
+	for _, dir := range directories {
+		err = watcher.Add(dir)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	<-done
 }
